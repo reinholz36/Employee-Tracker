84b884eed2bc0ae718f4e742aa3f77de8da74551
reinholz36 <dreinholz28@gmail.com>
2022-02-10 16:02:35 -0500

    added schema file and table sql

---
+++ db/schema.sql
@@ -0,0 +1,42 @@
+DROP DATABASE IF EXISTS employeetracker_db
+
+CREATE DATABASE employeetracker_db;
+
+USE employeetracker_db;
+
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employees;
+
+CREATE TABLE departments (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(6,0) NOT NULL,
+    department_id INTEGER, 
+    CONSTRAINT fk_departments
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employees (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER,
+    CONSTRAINT fk_roles
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE SET NULL,
+    CONSTRAINT fk_manager
+    FOREIGN KEY (manager_id)
+    REFERENCES employees(id)
+    ON DELETE SET NULL
+);
+
