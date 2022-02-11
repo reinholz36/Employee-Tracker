b32bcbf0b41a12779f408afc131ab33e8e69cb5a
reinholz36 <dreinholz28@gmail.com>
2022-02-11 11:45:01 -0500

    added seed data to seed.sql

---
+++ db/seeds.sql
@@ -0,0 +1,29 @@
+INSERT INTO departments (department_name)
+VALUE
+('Sales'),
+('Engineering'),
+('Finance'),
+('Legal');
+
+INSERT INTO roles (title, salary, department_id)
+VALUE
+('Sales Lead', 100000, 1),
+('Salesperson', 80000, 1),
+('Lead Engineer', 150000, 2),
+('Software Engineer', 120000, 2),
+('Account Manager', 160000, 3), 
+('Accountant', 125000, 3),
+('Legal Team Lead', 250000, 4),
+('Lawyer', 190000, 4);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUE
+('John', 'Doe', 1, null),
+('Mike', 'Chan', 2, 1),
+('Ashley', 'Rodriguez', 3, null),
+('Kevin', 'Tupik', 4, 3),
+('Kunal', 'Singh', 5, null),
+('Malia', 'Brown', 6, 5),
+('Sarah', 'Lourd', 7, null),
+('Tom', 'Allen', 8, 7);
+
