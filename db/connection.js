fd4de2b5a86a29351a7271d8ec9fd5c0c846d8ce
reinholz36 <dreinholz28@gmail.com>
2022-02-11 11:17:14 -0500

    udpated file name to index.js

--- db/connection.js
+++ db/connection.js
@@ -2,9 +2,14 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
     host: 'localhost',
+    port: '3001',
     user: 'root',
     password: 'root',
     database: 'employeetracker_db'
+});
+
+db.connect(function(err, response) {
+    (err) ? err : response;
 });
 
 module.exports = db;
---
+++ index.js
@@ -0,0 +1,4 @@
+const db = require('./db/connection');
+const inquirer = require('inquirer');
+const figlet = require('figlet');
+require('console.table');
