5ef2ffcb28eec72359c0e27229dec787591a0acb
reinholz36 <dreinholz28@gmail.com>
2022-02-12 18:48:12 -0500

    added starter icon, list, and function titles

--- index.js
+++ index.js
@@ -3,6 +3,128 @@ const inquirer = require('inquirer');
 const figlet = require('figlet');
 const consoleTable = require('console.table');
 
+const openingScreen = () => {
+
+    figlet('Hello World!!', function(err, data) {
+        if (err) {
+            console.log('Something went wrong...');
+            console.dir(err);
+            return;
+        }
+        console.log(data)
+        console.log(`\n`)
+    });
+}
+
+const startApp = async () => {
+    openingScreen();
+}
+
+const startQuestion = () => {
+    inquirer.prompt({
+        name: 'action',
+        type: 'rawlist',
+        message: 'What would you like to do?',
+        choices: [
+            'View All Employees',
+            'Add Employee',
+            'Remove Employee', 
+            'Update Employee Role', 
+            'View All Roles',
+            'Add Role',
+            'Remove Role',
+            'View All Departments',
+            'Add Department',
+            'Remove Department',
+            'End Program'
+        ]
+    })
+        .then((answer) => {
+            switch (answer.action) {
+
+                case 'View All Employees':
+                viewAllEmployees()
+                break;
+
+                case 'Add Employee':
+                addEmployee()
+                break;
+
+                case 'Remove Employee':
+                removeEmployee()
+                break;
+
+                case 'Update Employee Role':
+                updateEmployeeRole()
+                break;
+
+                case 'View All Roles':
+                viewAllRoles()
+                break;
+
+                case 'Add Role':
+                addRole()
+                break;
+
+                case 'Remove Role':
+                removeRole()
+                break;
+
+                case 'View All Departments':
+                viewAllDepartments()
+                break;
+
+                case 'Add Department':
+                addDepartment()
+                break;
+
+                case 'Remove Department':
+                removeDepartment()
+                break;
+
+                case 'End Program':
+                endProgram()
+                break;
+            }
+        })
+}
+
+viewAllEmployees()
+
+addEmployee()
+
+removeEmployee()
+
+updateEmployeeRole()
+
+viewAllRoles()
+
+addRole()
+
+removeRole()
+
+viewAllDepartments()
+
+addDepartment()
+
+removeDepartment()
+
+endProgram()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Figlet Title "Employee Manager"
 
 //What would you like to do? *helper text in dark gray (Use arrow keys)
@@ -45,3 +167,5 @@ const consoleTable = require('console.table');
     //Quit
 
   //*helper text in dark gray (Move up and down to reveal more choices)
+
+  startApp();
